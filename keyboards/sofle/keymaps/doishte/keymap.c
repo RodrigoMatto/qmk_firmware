3f780867e830b0f1a380bd77df34af1a6208c66c
Rodrigo Matto <rodrigodavid-@hotmail.com>
2023-02-09 19:30:40 -0300

    Create Soflev2/doishte keymap

--- keyboards/sofle/keymaps/doishte/keymap.c
+++ keyboards/sofle/keymaps/doishte/keymap.c
@@ -21,11 +21,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [0] = LAYOUT(
-  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_PIPE,
-  KC_ESC,   KC_Q,   KC_D,    KC_R,    KC_W,    KC_B,                      KC_J,    KC_F,    KC_U,    KC_P,   KC_COLN, KC_QUOT,
+  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_BSLS,
+  KC_F12,   KC_Q,   KC_D,    KC_R,    KC_W,    KC_B,                      KC_J,    KC_F,    KC_U,    KC_P,   KC_SCLN, KC_QUOT,
   KC_TAB,   KC_A,   KC_S,    KC_H,    KC_T,    KC_G,                      KC_Y,    KC_N,    KC_E,    KC_O,   KC_I,    KC_BSPC,
-  KC_LCTRL, KC_Z,   KC_X,    KC_M,    KC_C,    KC_V,   KC_MUTE,   KC_MPLY,KC_K,    KC_L,    KC_COMM, KC_DOT, KC_SLSH, KC_RGUI,
-                   KC_LGUI,  KC_LCTRL,KC_LALT, KC_ENT, MO(2),     MO(3),  KC_SPC,  KC_RSFT, KC_RCTRL, TG(1)
+  MO(2),    KC_Z,   KC_X,    KC_M,    KC_C,    KC_V,   KC_MUTE,   KC_MPLY,KC_K,    KC_L,    KC_COMM, KC_DOT, KC_SLSH, KC_RGUI,
+                    XXXXXXX, XXXXXXX, KC_LALT, KC_ENT, KC_LCTRL,  MO(3),  KC_SPC,  KC_RSFT, XXXXXXX, TG(1)
 ),
 /*
  * WORKMAN
